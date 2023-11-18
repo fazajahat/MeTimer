f08e63e0e2c353b0f7d51931d01daf8f5ad67641
raihanibagaskoro <raihanibagaskoro@gmail.com>
2023-11-18 20:13:41 +0700

    Onboarding done, json emotions list done

--- client-side/App.js
+++ client-side/App.js
@@ -2,6 +2,8 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LandingPage from './screens/LandingPage'
+import LoginPage from './screens/LoginPage'
+import Onboarding from './components/Onboarding/Onboarding';
 
 const Stack = createStackNavigator();
 
@@ -9,7 +11,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
-        <Stack.Screen name="LandingPage" component={LandingPage} />
+        <Stack.Screen name="Onboarding" component={Onboarding} />
       </Stack.Navigator>
     </NavigationContainer>
   );

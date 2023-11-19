9c5acafc460adc11616c2bc550336a6c38fff3e3
Dapow221 <141142350+Dapow221@users.noreply.github.com>
2023-11-19 19:27:43 +0700

    test

--- client-side/App.js
+++ client-side/App.js
@@ -1,16 +1,19 @@
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LandingPage from './screens/LandingPage'
-import LoginPage from './screens/LoginPage'
-import Onboarding from './components/Onboarding/Onboarding';
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LandingPage from "./screens/LandingPage";
+import LoginPage from "./screens/LoginPage";
+import Onboarding from "./components/Onboarding/Onboarding";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
         <Stack.Screen name="Onboarding" component={Onboarding} />
       </Stack.Navigator>
     </NavigationContainer>
